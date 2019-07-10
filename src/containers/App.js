7ab7e342ce722abe7feda546d8f2dc5457ef37b5
lolimay <lolimay@lolimay.cn>
2019-07-11 03:53:50 +0800

    Add description

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import styled from 'styled-components';
+
 import './App.css';
 
 import Header from './Header';
@@ -9,11 +12,17 @@ function App() {
   return (
     <React.Fragment>
       <Header />
+      <StyledP>在白板上写一个 0~9 之间的数字，然后机器会猜测该数字并显示在下方。</StyledP>
       <Card className='card-canvas'>
         <Fabric />
       </Card>
     </React.Fragment>
   );
 }
+
+const StyledP = styled.p`
+  max-width: 300px;
+  margin: 0 auto 20px;
+`;
 
 export default App;
