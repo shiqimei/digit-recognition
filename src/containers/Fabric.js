b30827287fed15d660db766389e41295129e699c
lolimay <lolimay@lolimay.cn>
2019-07-11 03:57:12 +0800

    Move fabricCanvas to global variable

--- src/containers/Fabric.js
+++ src/containers/Fabric.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-import { fabric } from 'fabric';
-
 class Fabric extends React.Component {
 	componentDidMount() {
-		const fabricCanvas = new fabric.Canvas('drawing', {
-			width: 300,
-			height: 300,
-			isDrawingMode: true,
-			freeDrawingBrush: {
-				width: 100
-			},
-			freeDrawingBrush: {
-				width: 10
-			}
-
-		});
+		const fabricCanvas = window.fabricCanvas;
 		const el = ReactDOM.findDOMNode(this);
 
 		fabricCanvas.initialize(el);
--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
+import { fabric } from 'fabric';
+
+// Global Variables
+window.fabricCanvas = new fabric.Canvas('drawing', {
+	width: 300,
+	height: 300,
+	isDrawingMode: true,
+	freeDrawingBrush: {
+		width: 100
+	},
+	freeDrawingBrush: {
+		width: 10
+	}
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
