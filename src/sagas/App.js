5c8cc1a4ef465a92254520bca1f718c083e1fddd
lolimay <lolimay@lolimay.cn>
2019-07-11 16:06:01 +0800

    Use canvasElement.toBlob(callback) directly

--- src/sagas/App.js
+++ src/sagas/App.js
@@ -1,12 +1,12 @@
 import { put, takeLatest, all } from 'redux-saga/effects';
 import { APP } from '../actions/actionTypes';
-import { dataURIToBlob, randomString } from '../utils';
+import { randomString } from '../utils';
 import { saveAs } from 'file-saver';
 
 const handleSaveImage = function* handleSaveImage({ canvas }) {
-	const dataURL = canvas.toDataURL( "image/png" );
-	const blob = dataURIToBlob(dataURL);
-	yield saveAs(blob, randomString(20));
+	canvas.toBlob(blob => {
+		saveAs(blob, `${ randomString(20) }.png`);
+	});
 }
 
 const root = function* root() {
