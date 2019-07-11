be98620b7812db5bb44d481a3de530eee73b46df
lolimay <lolimay@lolimay.cn>
2019-07-11 15:19:44 +0800

    Add util function randomString

---
+++ src/utils/randomString.js
@@ -0,0 +1,16 @@
+export function randomString(len) {
+	var buf = [],
+		chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+		charlen = chars.length;
+
+	for (var i = 0; i < len; ++i) {
+		buf.push(chars[getRandomInt(0, charlen - 1)]);
+	}
+
+	return buf.join("");
+}
+
+// inner-functions
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
