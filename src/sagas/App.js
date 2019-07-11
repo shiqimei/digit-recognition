78ca696c670c6d0eb705b17ab83c67e132d7678b
lolimay <lolimay@lolimay.cn>
2019-07-11 15:20:24 +0800

    Add file-saver package and save image to png file

--- src/sagas/App.js
+++ src/sagas/App.js
@@ -1,11 +1,12 @@
 import { put, takeLatest, all } from 'redux-saga/effects';
 import { APP } from '../actions/actionTypes';
-import { dataURIToBlob } from '../utils';
+import { dataURIToBlob, randomString } from '../utils';
+import { saveAs } from 'file-saver';
 
 const handleSaveImage = function* handleSaveImage({ canvas }) {
 	const dataURL = canvas.toDataURL( "image/png" );
 	const blob = dataURIToBlob(dataURL);
-	console.log(blob)
+	yield saveAs(blob, randomString(20));
 }
 
 const root = function* root() {
