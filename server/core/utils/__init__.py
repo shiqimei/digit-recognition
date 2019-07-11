3a2f360bb01c304a8358ddb88abb8c554992f0b9
lolimay <lolimay@lolimay.cn>
2019-07-11 23:00:47 +0800

    Add package utils.load_mnist

---
+++ server/core/utils/__init__.py
@@ -0,0 +1,4 @@
+"""
+Created by lolimay <lolimay@lolimay.cn>
+Last Updated 2019-07-11
+"""
--- server/core/utils/load_mnist.py
+++ server/core/utils/load_mnist.py
@@ -1,22 +1,20 @@
+"""
+Load MNIST dataset
+"""
+
 import os
 import struct
 import numpy as np
 
 def load_mnist(path, kind='train'):
-    """
-    Load MNIST dataset
-    """
     labels_path = os.path.join(path, '%s-labels-idx1-ubyte' % kind)
     images_path = os.path.join(path, '%s-images-idx3-ubyte' % kind)
     with open(labels_path, 'rb') as lbpath:
-        magic, n = struct.unpack('>II',
-                                 lbpath.read(8))
-        labels = np.fromfile(lbpath,
-        dtype=np.uint8)
+        magic, n = struct.unpack('>II', lbpath.read(8))
+        labels = np.fromfile(lbpath, dtype=np.uint8)
 
     with open(images_path, 'rb') as imgpath:
-        magic, num, rows, cols = struct.unpack('>IIII',
-                                               imgpath.read(16))
-        images = np.fromfile(imgpath,
-                             dtype=np.uint8).reshape(len(labels), 784)
+        magic, num, rows, cols = struct.unpack('>IIII', imgpath.read(16))
+        images = np.fromfile(imgpath, dtype=np.uint8).reshape(len(labels), 784)
+
     return images, labels
