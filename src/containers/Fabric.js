0fff1d7b0661a4684f39887318b83438e45a4b76
lolimay <lolimay@lolimay.cn>
2019-07-11 16:36:20 +0800

    Use dataURIBlob to generate blob

--- src/containers/Fabric.js
+++ src/containers/Fabric.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import { connect } from 'react-redux';
+import { dataURIToBlob } from  '../utils';
+import {
+	RecognizeRequest as RecognizeRequestAction
+} from '../actions/App';
+
+@connect(null, dispatch => ({
+	RecognizeRequest: blob => dispatch(RecognizeRequestAction(blob))
+}))
 
 class Fabric extends React.Component {
 	componentDidMount() {
+		const { RecognizeRequest } = this.props;
 		const fabricCanvas = window.fabricCanvas;
 		const el = ReactDOM.findDOMNode(this);
+		const blob = dataURIToBlob(fabricCanvas.toDataURL());
 		fabricCanvas.initialize(el);
+		fabricCanvas.on('mouse:up', e => RecognizeRequest(blob))
 	}
 
 	render() {
