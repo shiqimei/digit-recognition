bd4da09cf07bb9dba9531f00989176417ccddc01
lolimay <lolimay@lolimay.cn>
2019-07-13 01:29:50 +0800

    Complete the basic part of the Recognizing module

--- server/core/Recognizing/run_Recognize_Model.py
+++ server/core/Recognizing/run_Recognize_Model.py
@@ -3,10 +3,12 @@ Recognize handwritten digits with trained model
 """
 
 import torch
-import os
+import os, sys
 import torchvision
 import torchvision.transforms as transforms
 from Config import *
+sys.path.insert(0, os.path.abspath('..'))
+from utils.base64_to_tensor import base64_to_tensor
 
 # Use GPU if cuda is available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -20,31 +22,31 @@ def recognize(config):
     # evaluation mode
     net.eval()
 
-    # Load MNIST training dataset
-    test_dataset = torchvision.datasets.MNIST(root=config.dataset_root, 
-                                            train=False, 
-                                            transform=transforms.ToTensor())
-                                            
-    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
-                                            batch_size=config.batch_size, 
-                                            shuffle=False)
-    
-    # Evaluating
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for images, labels in test_loader:
-            images = images.reshape(-1, 28*28).to(device)
-            labels = labels.to(device)
-            outputs = net(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-        print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
+    while 1:
+        # Load image tensor from base64 string
+        image_tensor = base64_to_tensor(input())
+
+        # Reshape
+        image_tensor = image_tensor.view(-1, 28*28).to(device, dtype=torch.float)
+
+        torch.set_printoptions(linewidth=150)
+        
+        # Normalize
+        image_tensor = image_tensor.type(torch.float) / 255
+
+        [ output ] = net(image_tensor)
+
+        _, predicted = torch.max(output.data, -1)
+
+        # Predicted value
+        result = predicted.item()
+
+        # Output value
+        print(result)
 
 if __name__ == "__main__":
 
     config = Config()
 
-    # recognizing handwritten digits with trained model
+    # recognizing handwritten digit with trained model
     recognize(config)
