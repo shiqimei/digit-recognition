818295b7868b03990833d90ccd8aeefaa2819edb
lolimay <lolimay@lolimay.cn>
2019-07-13 05:05:42 +0800

    Apply  json in recognizeImage  API

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
 
 async function recognizeImage(base64String) {
-    return await axios.post('/api', base64String);
+    const { data } = await axios({
+        headers: {
+            'Content-Type': 'application/json'  
+        },
+        method: 'post',
+        url: '/api',
+        data: JSON.stringify(base64String)
+    });
+    return data;
 }
 
 export default {
